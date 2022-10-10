f34318e78a86375658afe14bbad6c1cc5aff40c6
Bagas Miftahun Na'im <bagasmiftahunaim@gmail.com>
2022-10-11 02:02:51 +0700

    Upload Project Vue

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# clothing-store
-Clothing sales store with transaction system and using Vue JS and Laravel programming languages💥
+# clothess-store
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderStore/>
+    <HeroStore/>
+    <BannerStore/>
+    <InstaStore/>
+    <LogoStore/>
+    <FooterStore/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderStore from "@/components/HeaderStore.vue";
+import HeroStore from "@/components/HeroStore.vue";
+import BannerStore from "@/components/BannerStore.vue";
+import InstaStore from "@/components/InstaStore.vue";
+import LogoStore from "@/components/LogoStore.vue";
+import FooterStore from "@/components/FooterStore.vue";
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderStore,
+    HeroStore,
+    BannerStore,
+    InstaStore,
+    LogoStore,
+    FooterStore
+  }
+}
+</script>
